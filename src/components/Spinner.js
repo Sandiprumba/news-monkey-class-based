640081997e0671d41c9960c09dd420d44dccd3c3
sandiprumba <sandiprumba.lama@gmail.com>
2023-10-30 18:38:52 +0800

    function based full code

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,13 +1,11 @@
 import React from "react";
 import loading from "./loading.gif";
 
-class Spinner extends React.Component {
-  render() {
-    return (
-      <div className="text-center">
-        <img className="my-3" src={loading} alt="loading" />
-      </div>
-    );
-  }
-}
+const Spinner = () => {
+  return (
+    <div className="text-center">
+      <img className="my-3" src={loading} alt="loading" />
+    </div>
+  );
+};
 export default Spinner;
